4f28995739b61c4a1f17141924dffe12b198a543
Rohan Dewan <35611893+fedcocoa@users.noreply.github.com>
2023-03-03 18:09:52 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log("Loaded from github");
+  const slideshowImages = document.querySelectorAll(".slideshow-img");
+  const slideshowInterval = 5000; // in milliseconds
+  let slideshowIndex = 0;
+
+  setInterval(() => {
+    console.log("tick");
+    slideshowImages[slideshowIndex].style.display = "none";
+    slideshowIndex = (slideshowIndex + 1) % slideshowImages.length;
+    slideshowImages[slideshowIndex].style.display = "block";
+  }, slideshowInterval);
